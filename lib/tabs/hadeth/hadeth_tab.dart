cf46d8d80c5948e05001aad8ef609e2207e761f3
Khadiga Abdelrahman <khadija15599@gmail.com>
2024-10-24 21:55:37 +0300

    finish home screen

---
+++ lib/tabs/hadeth/hadeth_tab.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class HadethTab extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+
+}
